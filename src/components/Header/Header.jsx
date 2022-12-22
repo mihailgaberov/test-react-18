71e8c0bff2253dead43acd475305cc89487114fa
Mihail Gaberov <mihail.gaberov@olx.com>
2022-12-22 12:04:33 +0100

    :sparkles: Add header, make grid responsive

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import './Header.scss'
+export const Header = () =>
+    (
+        <header>
+            <h1>Mickey Fashion Toys Inc.</h1>
+        </header>
+    );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
-import { HeadBanner } from '../../components/HeadBanner/HeadBanner';
-import { ItemsGrid } from '../../components/ItemsGrid/ItemsGrid';
+import {HeadBanner} from '../../components/HeadBanner/HeadBanner';
+import {ItemsGrid} from '../../components/ItemsGrid/ItemsGrid';
 import './Home.scss';
 import {v4 as uuidv4} from "uuid";
+import {Header} from "../../components/Header/Header";
 
 
 export const Home = () => (
     <main className='home'>
-        <header>Fashion Toys Header</header>
-        <HeadBanner />
+        <Header/>
+        <HeadBanner/>
         <ItemsGrid items={[
             {
                 name: "Toy 1",
@@ -45,7 +46,7 @@ export const Home = () => (
                 price: '$12',
                 imgName: 'toy6.png'
             }
-        ]} />
+        ]}/>
         <footer>Mickey Fashion Toys Inc.</footer>
     </main>
 );
